5dd2488ff836ec721262504c7d0b6e414ad491d5
Alex <AlexeyZ213@yandex.ru>
2022-12-27 11:32:10 +0300

    ProductSearch tests added

---
+++ UITests/UITests/ProductSearch/DriverMethods/ProductSearchMethods.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace UITests.ProductSearch.DriverMethods
+{
+internal class SearchMethods
+{
+    private readonly IWebDriver _webDriver;
+    private IWebElement _currentElement;
+
+    private static readonly By _categoryLinkXPath = By.XPath(".//a[contains(@href,'category')]"),
+                               _categoryMenuXPath = By.XPath("//div[@class='menu']"),
+                               _categoryTagXPath = By.XPath("//ul[contains(@class,'tag')]"),
+                               _searchBreadCrumbXPath = By.XPath("//*[@class='breadcrumb']"),
+                               _searchFormXPath = By.XPath("//form[@action='search']"),
+                               _searchInputXPath = By.XPath(".//*[@id='typeahead']"),
+                               _searchMenuSuggestionXPath = By.XPath(".//div[contains(@class,'tt-suggestion')]"),
+                               _searchMenuXPath = By.XPath(".//div[contains(@class,'tt-menu')]"),
+                               _searchOpenMenuXPath = By.XPath(".//div[contains(@class,'tt-open')]"),
+                               _searchResultContainerXPath = By.XPath("//div[contains(@class,'prdt-left')]"),
+                               _searchResultLinkXPath = By.XPath(".//a[contains(@href, 'product/')]"),
+                               _searchSubmitInputXPath = By.XPath("//input[contains(@class,'tt-hint')]");
+
+    public SearchMethods(IWebDriver webDriver)
+    {
+        _webDriver = webDriver;
+    }
+
+    public void SetCurrentElement(IWebElement webElement)
+    {
+        _currentElement = webElement;
+    }
+
+    public IWebElement GetSearchFormElement()
+    {
+        return _webDriver.FindElement(_searchFormXPath);
+    }
+
+    public IWebElement GetSearchInputElement()
+    {
+        return _webDriver.FindElement(_searchInputXPath);
+    }
+
+    public void FillInputElement(string searchQuery)
+    {
+        _currentElement.SendKeys(searchQuery);
+        WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+        wait.Until(e => _webDriver.FindElement(_searchFormXPath).FindElement(_searchOpenMenuXPath).Displayed);
+    }
+
+    public IWebElement GetSearchMenuElement()
+    {
+        return _currentElement.FindElement(_searchMenuXPath);
+    }
+
+    public int GetSearchMenuSuggesstionsCount()
+    {
+        var menuDataset = _currentElement.FindElements(_searchMenuSuggestionXPath);
+        return menuDataset.Count;
+    }
+
+    public void SubmitSearchInput()
+    {
+        _currentElement.FindElement(_searchSubmitInputXPath).SendKeys(Keys.Enter);
+    }
+
+    public string GetBreadCrumbText()
+    {
+        var breadCrumb = _webDriver.FindElement(_searchBreadCrumbXPath);
+        return breadCrumb.Text;
+    }
+
+    public ReadOnlyCollection<IWebElement> GetSearchResults()
+    {
+        return _webDriver.FindElements(_searchResultLinkXPath);
+    }
+
+    public IWebElement GetCategoryMenuElement()
+    {
+        return _webDriver.FindElement(_categoryMenuXPath);
+    }
+
+    public void SelectCategory(string baseCategory, string concreteCategory)
+    {
+        By baseCategoryXPath = By.XPath($".//a[contains(@href, 'category/{baseCategory}')]");
+        var baseCategoryElement = _currentElement.FindElement(baseCategoryXPath);
+
+        if (concreteCategory == baseCategory)
+        {
+            baseCategoryElement.Click();
+            return;
+        }
+        else
+        {
+            Actions builder = new Actions(_webDriver);
+            builder.MoveToElement(baseCategoryElement).Perform();
+        }
+
+        By concreteCategoryXPath = By.XPath($".//a[contains(@href, 'category/{concreteCategory}')]");
+        _currentElement.FindElement(concreteCategoryXPath).Click();
+    }
+
+    public string SelectSearchResultProduct()
+    {
+        string href = _currentElement.GetAttribute("href");
+        _currentElement.Click();
+        return href;
+    }
+
+    public string GetProductCategory()
+    {
+        var categoryTag = _webDriver.FindElement(_categoryTagXPath);
+        return categoryTag.FindElement(_categoryLinkXPath).Text;
+    }
+
+    public IWebElement GetSearchResultContainerElement()
+    {
+        return _webDriver.FindElement(_searchResultContainerXPath);
+    }
+}
+}
---
+++ UITests/UITests/ProductSearch/Tests/ProductSearchTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UITests.ProductSearch.DriverMethods;
+
+namespace ShopTests.UI.SearchingProductInCatalog.Tests
+{
+internal static class CustomWebElementAssert
+{
+    public static void IsEachWebElementEnabled(this Assert assert, ReadOnlyCollection<IWebElement> webElements)
+    {
+        foreach (var element in webElements)
+        {
+            Assert.IsTrue(element.Enabled, "Expected to see enabled web element");
+        }
+    }
+
+    public static void IsAppropriateBreadCrumb(this Assert assert, string breadCrumb, List<string> expectedElements)
+    {
+        foreach (var element in expectedElements)
+        {
+            Assert.IsTrue(breadCrumb.Contains(element), "Expected to contain each element in bread crumb");
+        }
+    }
+}
+
+[TestClass]
+public class SearchTests
+{
+    private SearchMethods _searchMethods;
+    private IWebDriver _webDriver;
+    private string _url = "http://shop.qatl.ru/";
+    private string _searchUrl = "search/";
+    private string _categoryUrl = "category/";
+    public TestContext TestContext { get; set; }
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver(Environment.GetEnvironmentVariable("CHROME_DIR"));
+        _webDriver.Navigate().GoToUrl(_url);
+        _webDriver.Manage().Window.Maximize();
+
+        _searchMethods = new SearchMethods(_webDriver);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        _webDriver.Quit();
+    }
+
+    [TestMethod]
+    [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "..\\..\\ProductSearch\\Configs\\ProductSearchCases.xml",
+                "TestSearch", DataAccessMethod.Sequential)]
+    public void SearchProducts()
+    {
+        _searchMethods.SetCurrentElement(_searchMethods.GetSearchFormElement());
+        var searchMenu = _searchMethods.GetSearchMenuElement();
+        Assert.IsFalse(searchMenu.Displayed, "Expected not to see search menu when nothing was given to search");
+
+        _searchMethods.SetCurrentElement(_searchMethods.GetSearchInputElement());
+        string query = TestContext.DataRow["Query"].ToString();
+        _searchMethods.FillInputElement(query);
+        Assert.IsTrue(searchMenu.Displayed, "Expected to see search menu when text was given to search");
+
+        int suggestionsCount = int.Parse(TestContext.DataRow["Count"].ToString());
+        _searchMethods.SetCurrentElement(searchMenu);
+        Assert.AreEqual(suggestionsCount, _searchMethods.GetSearchMenuSuggesstionsCount(),
+                        $"Expected to get {suggestionsCount} number of suggestions");
+
+        _searchMethods.SubmitSearchInput();
+        Assert.AreEqual($"{_url}{_searchUrl}", $"{_webDriver.Url.Split('?').First()}/",
+                        "Expected to switch to search result page");
+        Assert.That.IsAppropriateBreadCrumb(_searchMethods.GetBreadCrumbText(), new List<string> { query });
+        Assert.That.IsEachWebElementEnabled(_searchMethods.GetSearchResults());
+    }
+
+    [TestMethod]
+    [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "..\\..\\ProductSearch\\Configs\\ProductSearchCases.xml",
+                "TestCategory", DataAccessMethod.Sequential)]
+    public void SearchCategory()
+    {
+        _searchMethods.SetCurrentElement(_searchMethods.GetCategoryMenuElement());
+
+        string baseCategory = TestContext.DataRow["BaseCategory"].ToString();
+        string concreteCategory = TestContext.DataRow["ConcreteCategory"].ToString();
+        concreteCategory = (concreteCategory == "") ? baseCategory : concreteCategory;
+        _searchMethods.SelectCategory(baseCategory, concreteCategory);
+
+        Assert.AreEqual($"{_url}{_categoryUrl}{concreteCategory}", _webDriver.Url,
+                        "Expected to switch to the according category page");
+        var searchBreadCrumbText = _searchMethods.GetBreadCrumbText().ToLower();
+        Assert.That.IsAppropriateBreadCrumb(searchBreadCrumbText, new List<string> { baseCategory, concreteCategory });
+
+        var searchResults = _searchMethods.GetSearchResults();
+        if (searchResults.Count == 0)
+        {
+            var container = _searchMethods.GetSearchResultContainerElement();
+            var expectedText = TestContext.DataRow["Text"].ToString();
+            Assert.AreEqual(expectedText, container.Text, "Expected to get same search result text as input require");
+            return;
+        }
+
+        _searchMethods.SetCurrentElement(searchResults.First());
+        var productLink = _searchMethods.SelectSearchResultProduct();
+        Assert.AreEqual($"{productLink}", _webDriver.Url, "Expected to switch to concrete product page");
+
+        var actualCategory = _searchMethods.GetProductCategory().ToLower();
+        Assert.AreEqual(concreteCategory, actualCategory,
+                        "Ëxpected to get product that corresponds specified category");
+    }
+}
+}
